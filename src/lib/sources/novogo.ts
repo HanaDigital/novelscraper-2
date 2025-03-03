c0ee27fb4918b13d39ba926b1d000b00164db462
Shehryar <shehryar.dev@gmail.com>
2025-03-03 17:19:58 +0400

    Added two more sources

---
+++ src/lib/sources/novogo.ts
@@ -0,0 +1,25 @@
+import * as cheerio from 'cheerio';
+import { ChapterT, NovelSource, NovelSourceProps, NovelT } from "./types";
+import { invoke } from "@tauri-apps/api/core";
+import { hashString } from "../utils";
+import { testChapters } from "./test_chapters";
+
+export class Novgo extends NovelSource {
+
+	constructor({ id, name, tags, logo, url }: NovelSourceProps) {
+		super({ id, name, tags, logo, url });
+	}
+
+	async searchNovels(query: string): Promise<NovelT[]> {
+		return [];
+	}
+
+	async getNovelMetadata(novel: NovelT): Promise<NovelT> {
+		return novel;
+	}
+
+	async downloadNovel(novel: NovelT, batchSize: number, batchDelay: number, startFromChapterIndex = 0): Promise<ChapterT[]> {
+		const chapters = await this.downloadChapters(novel, batchSize, batchDelay, startFromChapterIndex);
+		return chapters;
+	}
+}
--- src/lib/sources/sources.ts
+++ src/lib/sources/sources.ts
@@ -1,5 +1,9 @@
-import NovelFullLogo from '@/assets/store/novelfull-logo.png'
+import NovelFullLogo from '@/assets/store/novelfull-logo.png';
+import NovgoLogo from '@/assets/store/novgo-logo.png';
+import LightNovelPubLogo from '@/assets/store/lightnovelpub-logo.png';
 import { NovelFull } from './novelfull';
+import { LightNovelPub } from "./lightnovelpub";
+import { Novgo } from "./novogo";
 
 export const SOURCES = {
 	"novelfull": new NovelFull({
@@ -9,6 +13,20 @@ export const SOURCES = {
 		logo: NovelFullLogo,
 		url: 'https://novelfull.com'
 	}),
+	"novgo": new Novgo({
+		id: 'novgo',
+		name: 'Novgo',
+		tags: ['Chinese, Korean'],
+		logo: NovgoLogo,
+		url: 'https://novogo.com'
+	}),
+	"lightnovelpub": new LightNovelPub({
+		id: 'lightnovelpub',
+		name: 'LightNovelPub',
+		tags: ['Japanese'],
+		logo: LightNovelPubLogo,
+		url: 'https://www.lightnovelpub.com'
+	})
 } as const;
 
 export type SourceIDsT = keyof typeof SOURCES;
